00f664c3af423a4df3c6712f6fb1e4afad030617
pencherek <54142013+pencherek@users.noreply.github.com>
2023-06-23 09:10:53 +0200

    Update test_perso.s

--- Simulation/test_perso.s
+++ Simulation/test_perso.s
@@ -1,10 +1,10 @@
-# Made by Axam
-# This code will light leds following if buttons are pressed or not
+# Made by Pencherek based on code made by Axam
+# This code will let the user interact with a compter, the leds show the value of it, one button to add 1 and one to sub 1
 init:
-	add x7 zero zero
-	li x8 0b11111111 
+	add x7 zero zero	#register for the compter
+	li x8 0b11111111 	#max number for the compter
 	# Set leds off
-	li   leds 0b11000111
+	li   leds 0b00000000
 	# Init timer target (as we don't support big int (LUI) nor shifts, add himself to himself = *2 each time)
 	  # x20 = 0x7ff = 2048
 	li   x20 0b11111111111
@@ -26,13 +26,15 @@ init:
 	add  x20 x20 x20
 	# Wait led state
 	li   x4  1
-	addi x5 zero 1
-	addi x6 zero 2
+ 	#prepare register for branching
+	addi x5 zero 1		#value test for btn1
+	addi x6 zero 2		#value test for btn2
 
 # Goal of the program
 	# 1) Wait a bit
 	# 2) Check buttons state
-	# 3) Light leds depending on button
+	# 3) Increment or decrement x7 upon button push
+ 	# 4) Show x7 value with the leds
 # Special
 	# Led(0) indicates if we are waiting (for measurement)
 main:
@@ -57,22 +59,23 @@ wait_end:
 
 # Any button is pressed if we did not branch
 btns_on:
-	beq btns x5 btn1
-	beq btns x6 btn2
-	
+	beq btns x5 btn1	#branch if btn1 was pressed
+	beq btns x6 btn2	#branch if btn2 was pressed
+	jal end
+ 
 btn1:
-	beq leds x8 leds_zero
+	beq leds x8 leds_zero	#reset compter if to big
 	addi x7 x7 1
 	jal  end
 
-leds_zero:
+leds_zero:			#set compter to 0
 	add x7 zero zero
 	jal  end
 
 btn2:
 	addi x7 x7 -1
 
-# No buttons are pressed
+# No buttons are pressed / show x7 value with leds
 end:
 	add   leds zero x7
 	jal  main
